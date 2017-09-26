2b2dcc4527909963172870bba247be74f75d020e
riz4l <rizki.fahrizal@gmail.com>
2017-09-26 09:13:06 +0700

    remove original table

--- application/modules/auth/views/index.php
+++ application/modules/auth/views/index.php
@@ -49,6 +49,7 @@
 		            <div class="box-header">
 		              <h3 class="box-title">
                     <button class="btn btn-sm btn-default" onclick="reload_table()"><i class="glyphicon glyphicon-refresh"></i></button>
+                    <a href="<?php echo base_url()?>auth/create_user" class="btn btn-info btn-sm"><i class="fa fa-plus"></i> Add</a>
                     Data Users
                   </h3>
 		            </div>
@@ -64,31 +65,6 @@
                       </div>
                     <?php } ?>
 
-      				    <!-- 		<table cellpadding=0 cellspacing=10>
-      							<tr>
-      								<th><?php echo lang('index_fname_th');?></th>
-      								<th><?php echo lang('index_lname_th');?></th>
-      								<th><?php echo lang('index_email_th');?></th>
-      								<th><?php echo lang('index_groups_th');?></th>
-      								<th><?php echo lang('index_status_th');?></th>
-      								<th><?php echo lang('index_action_th');?></th>
-      							</tr>
-      							<?php foreach ($users as $user):?>
-      								<tr>
-      						            <td><?php echo htmlspecialchars($user->first_name,ENT_QUOTES,'UTF-8');?></td>
-      						            <td><?php echo htmlspecialchars($user->last_name,ENT_QUOTES,'UTF-8');?></td>
-      						            <td><?php echo htmlspecialchars($user->email,ENT_QUOTES,'UTF-8');?></td>
-      									<td>
-      										<?php foreach ($user->groups as $group):?>
-      											<?php echo anchor("auth/edit_group/".$group->id, htmlspecialchars($group->name,ENT_QUOTES,'UTF-8')) ;?><br />
-      						                <?php endforeach?>
-      									</td>
-      									<td><?php echo ($user->active) ? anchor("auth/deactivate/".$user->id, lang('index_active_link')) : anchor("auth/activate/". $user->id, lang('index_inactive_link'));?></td>
-      									<td><?php echo anchor("auth/edit_user/".$user->id, 'Edit') ;?></td>
-      								</tr>
-      							<?php endforeach;?>
-      						</table> -->
-
                       <table id="table" class="table table-striped table-hover table-bordered" cellspacing="0" width="100%">
                           <thead>
                               <tr style="background-color:#e1e1e1;">
@@ -96,6 +72,7 @@
                                   <th width="15%">First Name</th>
                                   <th width="15%">Last Name</th>
                                   <th width="20%">Email</th>
+                                  <th width="20%">Group</th>
                                   <th width="12%">Status</th>
                                   <th width="5%">Actions</th>
                               </tr>
@@ -103,8 +80,6 @@
                           <tbody>
                           </tbody>
                       </table>
-
-      						  <p><?php echo anchor('auth/create_user', lang('index_create_user_link'))?> | <?php echo anchor('auth/create_group', lang('index_create_group_link'))?></p>
 					      </div>
             		<!-- /.box-body -->
           		</div>
